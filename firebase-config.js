24871083968733b197a953db831c1a83d8946233
alhalqat <alkhmalifawaz@gmail.com>
2026-03-26 15:57:49 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBzMkM4GOFAzdbCLq62c19BWzifobx9KKg",
+  authDomain: "halaqat-qaid.firebaseapp.com",
+  projectId: "halaqat-qaid",
+  storageBucket: "halaqat-qaid.firebasestorage.app",
+  messagingSenderId: "202520860190",
+  appId: "1:202520860190:web:2d85fa15d5c128c3d0400f"
+};
