0bf8ebde2e4a48b5c5626c8378fbcc85fc8c47dd
alhalqat <alkhmalifawaz@gmail.com>
2026-05-22 21:10:39 +0300

    security: extract Firebase config to separate file js/firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+// إعدادات Firebase - ملف منفصل
+// ملاحظة: مفتاح API في المتصفح طبيعي، الأمان من Firestore Rules + Auth
+const firebaseConfig = {
+  apiKey: "AIzaSyAsQQ9GGfZ0DmS4V5Fqv49rypXDEeoxCaA",
+  authDomain: "alhalqat-6ac9d.firebaseapp.com",
+  databaseURL: "https://alhalqat-6ac9d-default-rtdb.firebaseio.com",
+  projectId: "alhalqat-6ac9d",
+  storageBucket: "alhalqat-6ac9d.firebasestorage.app",
+  messagingSenderId: "680568855745",
+  appId: "1:680568855745:web:b8adfa8598dfd6d801c840",
+  measurementId: "G-4L9H2WQS60"
+};
